e53971641a76cac75757fdebf3f96db448280152
Marlon Strong <tsenane14@gmail.com>
2021-11-22 02:36:35 +0200

    added styles  to home

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { Container, Header } from "./styles";
+
+const Home = () => {
+  return (
+    <Container>
+      <Header>
+        <Text>Hello</Text>
+      </Header>
+    </Container>
+  );
+};
+
+export default Home;
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components/native'
+
+
+export const Container=styled.View`
+flex:1;
+align-items: center;
+`
+export const Header=styled.View`
+flex-direction: row;
+`
+
+export const WelcomeContainer=styled.View`
+
+`
+
+export const AvatarContainer=styled.View`
+
+`
