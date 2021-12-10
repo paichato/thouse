e492f3adf3ab6aa6e38083c740dbd7746e7c122b
Marlon Strong <tsenane14@gmail.com>
2021-12-10 23:19:53 +0200

    added fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,28 @@
+import {
+  useFonts,
+  Nunito_700Bold,
+  Nunito_400Regular,
+} from "@expo-google-fonts/nunito";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Routes from "./src/routes";
 import Home from "./src/screens/Home";
 
 export default function App() {
-  return <Routes />;
+  let [fontsLoaded] = useFonts({
+    Nunito_700Bold,
+    Nunito_400Regular,
+  });
+
+  if (fontsLoaded) {
+    return <Routes />;
+  } else {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
