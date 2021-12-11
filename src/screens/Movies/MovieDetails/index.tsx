f09ee539891290cde7048096c6e4400ade40f056
Marlon Strong <tsenane14@gmail.com>
2021-12-11 02:08:50 +0200

    changed structure for details

--- src/screens/Movies/MovieDetails/index.tsx
+++ src/screens/Movies/MovieDetails/index.tsx
@@ -1,10 +1,56 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import colors from "../../../lib/colors";
+import { BackArrow, Greeting } from "../Home/styles";
+import {
+  Container,
+  HeaderContainer,
+  MoviesDetailsContainer,
+  MoviesExtraDetailsContainer,
+  MoviesExtraDetailsItem,
+  Title,
+} from "./styles";
 
 export default function MoviesDetails() {
+  const uri =
+    "https://br.web.img2.acsta.net/pictures/21/05/10/15/32/2425639.png";
+
   return (
-    <View>
-      <Text>Hello</Text>
-    </View>
+    <Container>
+      <HeaderContainer>
+        <TouchableOpacity style={{ marginRight: "25%" }}>
+          <BackArrow name="md-chevron-back" size={24} color={colors.white} />
+        </TouchableOpacity>
+        <Title>Detalhes do filme</Title>
+      </HeaderContainer>
+      <MoviesDetailsContainer>
+        <Image
+          source={{ uri }}
+          style={{
+            height: RFValue(240),
+            width: RFValue(200),
+            borderRadius: 20,
+          }}
+        />
+        <MoviesExtraDetailsContainer>
+          <MoviesExtraDetailsItem>
+            <Text>Hello</Text>
+            <Greeting>hello</Greeting>
+            <Title>Hello</Title>
+          </MoviesExtraDetailsItem>
+          <MoviesExtraDetailsItem>
+            <Text>Hello</Text>
+            <Greeting>hello</Greeting>
+            <Title>Hello</Title>
+          </MoviesExtraDetailsItem>
+          <MoviesExtraDetailsItem>
+            <Text>Hello</Text>
+            <Greeting>hello</Greeting>
+            <Title>Hello</Title>
+          </MoviesExtraDetailsItem>
+        </MoviesExtraDetailsContainer>
+      </MoviesDetailsContainer>
+    </Container>
   );
 }
--- src/screens/Movies/MovieDetails/styles.ts
+++ src/screens/Movies/MovieDetails/styles.ts
@@ -1,16 +1,59 @@
 import styled from "styled-components/native";
+import { TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import colors from "../../../lib/colors";
+import fonts from "../../../lib/fonts";
+import { RFValue } from "react-native-responsive-fontsize";
 
 export const Container=styled.View`
 flex:1;
 width: 100%;
+align-items: center;
+justify-content: center;
+padding: ${RFValue(20)}px;
+background-color: ${colors.bg};
 `
 export const HeaderContainer=styled.View`
+flex-direction: row;
+align-items: center;
+width: 100%;
+`
+export const MoviesDetailsContainer=styled.View`
+flex-direction: row;
+width: 100%;
+align-self:center;
 
 `
-export const HeaderContainer=styled.View`
+export const MoviesExtraDetailsContainer=styled.View`
+
+width: 40%;
+align-items: center;
+justify-content:center;
+justify-content:space-between;
+/* height: ${RFValue(240)}px; */
 
 `
+export const MoviesExtraDetailsItem=styled.View`
+/* flex-direction: row; */
+align-items: center;
+/* justify-content:space-around; */
+width: ${RFValue(70)}px;
+height: ${RFValue(70)}px;
+border: 0.25px solid ${colors.white};
+border-radius:5px;
+`
 
-export const HeaderContainer=styled.View`
+export const MovieTitle=styled.View`
+color:${colors.white};
+font-size:18px;
+font-family:${fonts.bold};
+`
+export const CloseIcon=styled(Ionicons)`
+margin-right:8px;
+`
 
+export const Title=styled.Text`
+color:${colors.white};
+/* font-weight: bold; */
+font-family:${fonts.bold};
 `
